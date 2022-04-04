b580bf511431a5d6fd6519a313b210bc9077c74c
Kumar Shreyas <kumar.shreyas@leadsquared.com>
2022-04-04 13:02:59 +0530

    commit updated code for Keshav

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksService:TasksService){}
-    @Get()
-    getAllTasks(){
-        return this.tasksService.readAllTasks()
-    }
+  constructor(private tasksService: TasksService) {}
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.readAllTasks();
+  }
+  @Post()
+  addTask(title: string, description: string): Task {}
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable()
 export class TasksService {
-    private tasks=["blh"];
-    readAllTasks(){
-return this.tasks;
-    }
+  private tasks: Task[] = [];
+  readAllTasks(): Task[] {
+    return this.tasks;
+  }
+  addTask(title: string, description: string): Task {
+    const task: Task = {
+      id: 'UniqueId',
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
 }
